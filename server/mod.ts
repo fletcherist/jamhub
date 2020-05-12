413aee4271e9e79dd3cf010c0ae097e39f847c15
fletcherist <fletcherist@yandex.ru>
2020-05-12 11:03:39 +0300

    fix: error with server.listen()

--- server/mod.ts
+++ server/mod.ts
@@ -4,7 +4,7 @@ import { delay } from "https://deno.land/std/async/delay.ts";
 const port: number = Number(Deno.args[0]) || 8080;
 while (true) {
   try {
-    await server(port).listen();
+    await server(port).run();
   } catch (error) {
     await delay(1000);
     console.error(error);
